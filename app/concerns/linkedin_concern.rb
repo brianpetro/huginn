fd7dd4befd108e4940db2316ba4e69b2e6ceaa71
Brian Petro <brian@plexm.com>
2015-10-13 14:43:15 -0400

    finish LinkedIn service

--- app/concerns/linkedin_concern.rb
+++ app/concerns/linkedin_concern.rb
@@ -1,5 +1,6 @@
-module LinkedInConcern
+module LinkedinConcern
   extend ActiveSupport::Concern
+  require 'linkedin'
 
   included do
     include Oauthable
@@ -24,13 +25,18 @@ module LinkedInConcern
   end
 
   def linkedin
-    LinkedIn.configure do |config|
-      config.consumer_key = linkedin_consumer_key
-      config.consumer_secret = linkedin_consumer_secret
-      config.oauth_token = linkedin_oauth_token
-      config.oauth_token_secret = linkedin_oauth_token_secret
+    client = LinkedIn::Client.new(linkedin_consumer_key, linkedin_consumer_secret)
+    client.authorize_from_access(linkedin_oauth_token, linkedin_oauth_token_secret)
+    client  
+  end
+
+  module ClassMethods
+    def linkedin_dependencies_missing
+      if ENV['LINKEDIN_OAUTH_KEY'].blank? || ENV['LINKEDIN_OAUTH_SECRET'].blank?
+        "## Set LINKEDIN_OAUTH_KEY and LINKEDIN_OAUTH_SECRET in your environment to use LinkedIn Agents."
+      elsif !defined?(LinkedIn) || !Devise.omniauth_providers.include?(:linkedin)
+        "## Include the `linkedin` and `omniauth-linkedin` gems in your Gemfile to use LinkedIn Agents."
+      end
     end
-    
-    LinkedIn::Client.new
   end
 end
